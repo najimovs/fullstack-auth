14615ab5fae108d260bc4388a3abd0c642647a07
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-03-05 19:06:06 +0500

    Add cors and cookie parsers

--- api/src/index.js
+++ api/src/index.js
@@ -7,7 +7,37 @@ const GOOGLE_CLIENT_ID = process.env.GOOGLE_AUTH_CLIENT_ID
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_AUTH_CLIENT_SECRET
 const REDIRECT_URI = `http://localhost:${ PORT }/auth/callback`
 
-console.log( PORT )
-console.log( GOOGLE_CLIENT_ID )
-console.log( GOOGLE_CLIENT_SECRET )
-console.log( REDIRECT_URI )
+const usersDb = {}
+const sessions = {}
+
+const fastify = Fastify( {
+	logger: true,
+} )
+
+await fastify.register( cors, {
+	origin: [
+		"http://localhost:5173",
+		"http://127.0.0.1:5173",
+	],
+	credentials: true,
+} )
+
+await fastify.register( cookie )
+
+fastify.get( "/", async function handler ( request, reply ) {
+
+	return {
+		hello: "world"
+	}
+} )
+
+try {
+
+	await fastify.listen( { port: PORT } )
+}
+catch ( err ) {
+
+	fastify.log.error( err )
+
+	process.exit( 1 )
+}
