f3b41942dba9d90b085ec186dc357a0b27bc7b14
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-03-05 18:31:03 +0500

    Add env variables

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,13 @@
-console.log( "API" )
+import Fastify from "fastify"
+import cors from "@fastify/cors"
+import cookie from "@fastify/cookie"
+
+const PORT = parseInt( process.env.PORT || 3_000 )
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_AUTH_CLIENT_ID
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_AUTH_CLIENT_SECRET
+const REDIRECT_URI = `http://localhost:${ PORT }/auth/callback`
+
+console.log( PORT )
+console.log( GOOGLE_CLIENT_ID )
+console.log( GOOGLE_CLIENT_SECRET )
+console.log( REDIRECT_URI )
